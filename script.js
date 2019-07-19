102da1e0abfdcbefb071c710cad2b0c04125dc49
natyele <natyelebertolanii@gmail.com>
2019-07-19 16:24:19 -0300

    adicionar atividade de js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-let idade = Number(prompt("Digite sua idade"));
-let aposentado = prompt("voce e aposentado?")
-let estudando = prompt("voce e estudadante?")
+let idade = Number(prompt("digite sua idade"));
 
-if(idade >= 60 || aposentado ===  "sim"){
- alert("ENTRADA GRATIS");
-} else if (idade >= 0 && idade <= 12 ) {
-    alert("ENTRADA:10 REAIS");
-}else if (idade >= 13 && idade <= 17 ){
-    alert("ENTRADA:15 REAIS");
-}else if (idade >= 18 && idade >= 59){
-    alert("ENTRADA 35 REAIS")
-}else {(idade <=18 && idade >= 59 && estudando === "sim")
-    alert("MEIA ENTRADA 17.50 REAIS")
-}
+if(idade >=25 && idade <= 60){
+    let salario = Number(prompt("digite seu salario"));
+        if (salario  >=2000){
+            let valor = Number(prompt("digite o valor do emprestimo"))
+                if(valor <= 10*salario){
+                    console.log("Emprestimo aprovado")
+                }else{
+                    console.log("Valor não permitido")
+                }
+            }else{
+                console.log("Salário não permitido para empréstimo, precisa ser no minimoo  2.000")
+            }
+        }else{
+            console.log("Idade não permitida para empréstimo")
+        }
+
+
+
+ 
 
 
           
