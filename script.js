623de7493d30b87a5e44d96b2e1eb50545c44d5f
natyele <natyelebertolanii@gmail.com>
2019-07-19 17:03:21 -0300

    adicionar atividade de js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let idade = Number(prompt("digite sua idade"));
+/*/let idade = Number(prompt("digite sua idade"));
 
 if(idade >=25 && idade <= 60){
     let salario = Number(prompt("digite seu salario"));
@@ -17,8 +17,28 @@ if(idade >=25 && idade <= 60){
         }
 
 
+        CALLENDARIO */
 
- 
 
 
-          
+let numero = Number(prompt("digite um numero de 1 a 7 e descubra qual e o dia semanal"))
+
+ if  (numero ==1){
+    console.log("O dia 1 é domingo!")
+    }else if (numero==2){
+    console.log("O dia 2 é segunda-feira!")
+    }else if (numero==3){
+     console.log("O dia 3 é terça-feira!")
+    }else if (numero==4){
+     console.log("O dia 4 é quarta-feira!")
+    }else if (numero==5){
+     console.log("O dia 5 é quinta-feira!")
+    }else if (numero==6){
+     console.log("O dia 6 é sexta!")
+    }
+    else  if(numero ==7){
+     console.log("O dia 7 é sabado!")
+    }
+    else{
+        console.log("Digitou um numero invalido")
+    }
