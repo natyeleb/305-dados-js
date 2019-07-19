91a8a483429bbeaa1adf8f4b1fc1cdc393a9ed35
natyele <natyelebertolanii@gmail.com>
2019-07-19 17:13:13 -0300

    adicionar exercicio 1

--- script.js
+++ script.js
@@ -21,24 +21,41 @@ if(idade >=25 && idade <= 60){
 
 
 
-let numero = Number(prompt("digite um numero de 1 a 7 e descubra qual e o dia semanal"))
+// let numero = Number(prompt("digite um numero de 1 a 7 e descubra qual e o dia semanal"))
 
- if  (numero ==1){
-    console.log("O dia 1 é domingo!")
-    }else if (numero==2){
-    console.log("O dia 2 é segunda-feira!")
-    }else if (numero==3){
-     console.log("O dia 3 é terça-feira!")
-    }else if (numero==4){
-     console.log("O dia 4 é quarta-feira!")
-    }else if (numero==5){
-     console.log("O dia 5 é quinta-feira!")
-    }else if (numero==6){
-     console.log("O dia 6 é sexta!")
-    }
-    else  if(numero ==7){
-     console.log("O dia 7 é sabado!")
-    }
-    else{
-        console.log("Digitou um numero invalido")
-    }
+//  if  (numero ==1){
+//     console.log("O dia 1 é domingo!")
+//     }else if (numero==2){
+//     console.log("O dia 2 é segunda-feira!")
+//     }else if (numero==3){
+//      console.log("O dia 3 é terça-feira!")
+//     }else if (numero==4){
+//      console.log("O dia 4 é quarta-feira!")
+//     }else if (numero==5){
+//      console.log("O dia 5 é quinta-feira!")
+//     }else if (numero==6){
+//      console.log("O dia 6 é sexta!")
+//     }
+//     else  if(numero ==7){
+//      console.log("O dia 7 é sabado!")
+//     }
+//     else{
+//         console.log("Digitou um numero invalido")
+//     }
+
+
+let idade = Number(prompt("Digite sua idade"));
+let aposentado = prompt("voce e aposentado?")
+let estudando = prompt("voce e estudadante?")
+
+if(idade >= 60 || aposentado ===  "sim"){
+ alert("ENTRADA GRATIS");
+} else if (idade >= 0 && idade <= 12 ) {
+    alert("ENTRADA:10 REAIS");
+}else if (idade >= 13 && idade <= 17 ){
+    alert("ENTRADA:15 REAIS");
+}else if (idade >= 18 && idade >= 59){
+    alert("ENTRADA 35 REAIS")
+}else {(idade <=18 && idade >= 59 && estudando === "sim")
+    alert("MEIA ENTRADA 17.50 REAIS")
+}
